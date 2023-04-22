7c181baea873eb8135cf8d38f8557baee783fc3b
arkadyCh <arkadychakhalyan@gmail.com>
2023-04-22 14:37:43 +0400

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { App } from './App/App';
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
